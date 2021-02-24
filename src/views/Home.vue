945f00a7dc241efb5580d7a165d569637d34d0dd
ll799268 <1010023095@qq.com>
2021-02-24 11:31:12 +0800

    add css and serchInput

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div ref="container" class="flex-center clearfix">
-    home
+    <form action="#">
+      <input type="search" placeholder="请输入..." name="search" />
+    </form>
   </div>
 </template>
 
@@ -12,9 +14,6 @@ export default {
     return {
      
     }
-  },
-  mounted () {
-    console.log(countStr('老我的我你老发卡量刚老看到了公老交卡的很快过来哈剋老公'));
   }
 }
 </script>
