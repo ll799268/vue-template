6e5a4888df3ca472217339579310e112ec428f10
ll799268 <1010023095@qq.com>
2021-03-24 17:03:15 +0800

    format code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,19 @@
 <template>
-  <div ref="container" class="flex-center clearfix">
-    <form action="#">
-      <input type="search" placeholder="请输入..." name="search" />
-    </form>
+  <div>
+
   </div>
 </template>
 
 <script>
-import { getStyle } from 'libs/handleElement'
 export default {
   data() {
     return {
-      str: true
-    }
-  },
-  created () {
 
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
- 
+
 </style>
