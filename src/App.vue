938f64c819e130fa6885d64352475a98b05ab785
ll799268 <1010023095@qq.com>
2021-07-26 11:28:00 +0800

    vue change Object item

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,8 @@
   <router-view />
 </template>
 <script>
-import person from 'modules/Person'
 export default {
-  created () {
-    person.login({ userId: 1 })
-    console.log(person);
-  }
+  
 }
 </script>
 <style lang="scss">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,38 @@
 <template>
-  <div>
-    123
+  <div class="warpper">
+    <ul>
+      <li v-for="(item, index) of items" 
+        :key="index">{{ item }}</li>
+    </ul>
+    <button @click="addItems">add</button>
   </div>
 </template>
 
 <script>
-import person from 'modules/Person'
+import Vue from 'vue'
 export default {
   data() {
     return {
-
-    }
+      items: {
+        a: 1,
+        b: 2,
+      },
+    };
   },
-  created () {
-    console.log(person);
+  methods: {
+    addItems() {
+      this.items.c = 3
+
+      this.$forceUpdate()
+      // this.items = Object.assign({}, this.items, {
+      //   c: 3
+      // })
+
+      // Vue.set(this.items, 'c', 3)
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
