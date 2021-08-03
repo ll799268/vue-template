e510d6e96cad8e1c5ce36c7d114f803cac46a06a
ll799268 <1010023095@qq.com>
2021-08-03 11:14:07 +0800

    add ways

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -146,6 +146,41 @@ export const openWindow = (url, windowName, width, height) => {
  * @returns 
  */
 export const randomHexColorCode = () => {
-  let n = (Math.random() * 0xfffff * 1000000).toString(16);
-  return '#' + n.slice(0, 6);
+  let n = (Math.random() * 0xfffff * 1000000).toString(16)
+  return '#' + n.slice(0, 6)
 }
+
+/**
+ * 页面插入多条数据
+ * @param {Element} parentDom 要插入的父节点
+ * @param {Number} onceNumber 一次插入几条数据
+ */
+export const insertManyRecord = (parentDom, onceNumber=20) => {
+  setTimeout(() => {
+    // 插入十万条数据
+    const total = 100000
+    // 插入数据需要的次数
+    const loopCount = Math.ceil(total / onceNumber)
+    let countOfRender = 0
+    // 添加数据的方法
+    function add() {
+      const fragment = document.createDocumentFragment()
+      for(let i = 0; i < onceNumber; i++) {
+        const li = document.createElement('li')
+        li.innerText = Math.floor(Math.random() * total)
+        fragment.appendChild(li)
+      }
+      parentDom.appendChild(fragment)
+      countOfRender += 1
+      loop()
+    }
+    function loop() {
+      if(countOfRender < loopCount) {
+        window.requestAnimationFrame(add)
+      }
+    }
+    loop()
+  }, 0)
+}
+
+
