3405073af9444f5ad9c3ddd703dca459a5ccaf30
ll799268 <1010023095@qq.com>
2021-11-21 18:32:25 +0800

    format throttle

--- src/libs/ThrottleAndDebounce.js
+++ src/libs/ThrottleAndDebounce.js
@@ -1,16 +1,43 @@
 /**
- * 节流
+ * * 节流
  * @param {*} func 执行函数
  * @param {*} delay 节流时间,毫秒
+ * @param {*} leading 指定调用在节流开始前
+ * @returns 
  */
-export const throttle = (func, delay) => {
+export const throttle = (func, delay = 0, leading = true) => {
+  // ways1 相差时间戳比较
+  // let timer = null,
+  //   last // 上一次执行时间
+  // return function () {
+  //   const now = Date.now() // 现在的时间
+  //   if (last && now < last + delay) {
+  //     if (leading) return
+  //     clearTimeout(timer)
+  //     timer = setTimeout(() => {
+  //       last = now
+  //       func.apply(this, arguments)
+  //     }, delay)
+  //   } else {
+  //     last = now
+  //     func.apply(this, arguments)
+  //   }
+  // }
+
+  // ways2 是否存在定时器比较
   let timer = null
   return function () {
+    if (leading) {
+      func.apply(this, arguments)
+      leading = false
+    }
     if (!timer) {
       timer = setTimeout(() => {
         func.apply(this, arguments)
-        // 或者直接 func()
-        timer = null
+        if (!leading) {
+          timer = null
+          leading = false
+        }
       }, delay)
     }
   }
