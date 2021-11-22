0cc54f047f1630521e6eb736d1134262daaac444
ll799268 <1010023095@qq.com>
2021-11-22 17:21:31 +0800

    format code

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -1,37 +1,4 @@
 /**
- * 动态引入js
- * @param {String} src 
- */
-export const injectScript = src => {
-  let s = document.createElement('script');
-  s.type = 'text/JavaScript'
-  s.async = true
-  s.src = src
-  const t = document.getElementsByTagName('script')[0];
-  t.parentNode.insertBefore(s, t)
-}
-
-/**
- * 异步加载script
- */
-export const loadScript = (url, callback) => {
-  let oscript = document.createElement('script');
-  if (oscript.readyState) { // ie8及以下版本
-    oscript.onreadystatechange = function () {
-      if (oscript.readyState === 'complete' || oscript.readyState === 'loaded') {
-        callback()
-      }
-    }
-  } else {
-    oscript.onload = function () {
-      callback()
-    }
-  }
-  oscript.src = url
-  document.body.appendChild(oscript)
-}
-
-/**
  * 根据url地址下载
  * @param {String} url 
  */
