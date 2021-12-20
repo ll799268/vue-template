e0304ee6eee7ee023f22fdf7eb7795cb9bd2cf5f
ll799268 <1010023095@qq.com>
2021-12-20 17:07:58 +0800

    add vue tool

---
+++ src/libs/vueNativeTool.js
@@ -0,0 +1,9 @@
+
+/**
+ * 是否兼容此对象
+ * @param {object} Ctor 原生对象 eg: Promise
+ * @returns 
+ */
+export function isNative (Ctor) {
+  return typeof Ctor === 'function' && /native code/.test(Ctor.toString())
+}
